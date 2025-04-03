32e1ef1296986600d51b67f7d4c76820018c2bb2
Chinmay <chmungekar007@gmail.com>
2025-04-03 14:02:51 +0530

    First Version

---
+++ trial.js
@@ -0,0 +1,11 @@
+// Print to console
+console.log("Hello, World!");
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Call the function and log the result
+console.log("Sum of 5 and 3 is: " + addNumbers(5, 3));
+ 
